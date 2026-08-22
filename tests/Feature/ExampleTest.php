454a36cb81f764dd7907e58814938c2b1112fe46
yandev2 <ryanhappyjalay2461@gmail.com>
2026-08-23 03:13:32 +0700

    upgrade php 8.4 new

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -11,8 +11,6 @@ class ExampleTest extends TestCase
 
     public function test_the_application_returns_a_successful_response(): void
     {
-        $this->withoutVite();
-
         $response = $this->get('/');
 
         $response->assertStatus(200);
